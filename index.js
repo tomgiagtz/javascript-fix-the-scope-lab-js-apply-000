c20460246de082d808ce489feccb8661b99b54ca
Tom Giagtzoglou <tom.giagtzoglou@gmail.com>
2018-08-05 19:20:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
-var animal = 'dog'
 
-function myAnimal() {
-  return animal
-}
+
 
 function yourAnimal() {
   // How can we make sure that this function
@@ -10,6 +7,14 @@ function yourAnimal() {
   // P.S.: You can't just hard-code 'cat' below
   return animal
 }
+
+var animal = 'dog'
+
+function myAnimal() {
+  return animal
+}
+
+
 
 function add2(n) {
   const two = 2
